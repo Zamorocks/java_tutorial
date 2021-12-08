18a56e0299090146ee14694e17ad90ff5005e067
Christian Zamora <christian.zamora@wgu.edu>
2021-12-08 13:37:18 -0800

    switch cases

--- src/com/pluralsight/organized/Main.java
+++ src/com/pluralsight/organized/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Do While
-        Scanner scanner = new Scanner(System.in);
+        /*--------------------------- DO...WHILE --------------------------------*/
+//        Scanner scanner = new Scanner(System.in);
 
         /*System.out.println("Enter a number: ");
         int val = scanner.nextInt();
@@ -16,13 +16,31 @@ public class Main {
             val = scanner.nextInt();
         }*/
 
-        int value;
-        do{
-            System.out.println("Enter a number: ");
-            value = scanner.nextInt();
+//        int value;
+//        do{
+//            System.out.println("Enter a number: ");
+//            value = scanner.nextInt();
+//        }
+//        while(value != 5);
+//        System.out.println("Got 5!");
+//    }
+
+        /*--------------------------- SWITCH CASES --------------------------------*/
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a command: ");
+        String text = input.nextLine();
+
+        switch(text) {
+            case "start":
+                System.out.println("Machine started!");
+                break;
+            case "stop":
+                System.out.println("Machine stopped.");
+            default:
+                System.out.println("Command not recognized");
         }
-        while(value != 5);
-        System.out.println("Got 5!");
 
     }
 }
