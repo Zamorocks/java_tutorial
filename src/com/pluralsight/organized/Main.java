a2489a0c45f63c0cc9842899af25bf7ce5b4036c
Christian Zamora <christian.zamora@wgu.edu>
2021-12-08 16:11:39 -0800

    arrays lessons

--- src/com/pluralsight/organized/Main.java
+++ src/com/pluralsight/organized/Main.java
@@ -6,41 +6,79 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*--------------------------- DO...WHILE --------------------------------*/
-//        Scanner scanner = new Scanner(System.in);
+        /* -------------------------------------------------------------- ARRAYS --------------------------------------------------------------*/
 
-        /*System.out.println("Enter a number: ");
-        int val = scanner.nextInt();
-        while(val != 5) {
-            System.out.println("Enter a number: ");
-            val = scanner.nextInt();
-        }*/
-
-//        int value;
-//        do{
-//            System.out.println("Enter a number: ");
-//            value = scanner.nextInt();
+//        int value = 7;
+//        int[] values;
+//        values = new int[3];
+//        System.out.println(values[0]);
+//
+//        values[0] = 10;
+//        values[1] = 11;
+//        values[2] = 12;
+//
+////        System.out.println(values[0]);
+////        System.out.println(values[1]);
+////        System.out.println(values[2]);
+//
+//        for(int i = 0; i < values.length; i++) {
+//            System.out.println(values[i]);
 //        }
-//        while(value != 5);
-//        System.out.println("Got 5!");
-//    }
+//
+//        int[] numbers = {5, 6, 7};
+//
+//        for(int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
 
-        /*--------------------------- SWITCH CASES --------------------------------*/
+        /* ---------------------------------------------------- ARRAYS OF STRINGS ----------------------------------------------------*/
 
-        Scanner input = new Scanner(System.in);
+//        String[] fruits = {"apple", "banana", "pear", "kiwi"};
+//
+//        for(String fruit: fruits) {
+//            System.out.println(fruit);
+//        }
+//
+//        int value = 0;
+//
+//        String text = null;
+//        System.out.println(text);
+//
+//        String[] texts = new String[2];
+//        System.out.println(texts[0]);
+//
 
-        System.out.println("Please enter a command: ");
-        String text = input.nextLine();
+        /* -------------------------------------------------- MULTI-DIMENSIONAL ARRAYS --------------------------------------------------*/
 
-        switch(text) {
-            case "start":
-                System.out.println("Machine started!");
-                break;
-            case "stop":
-                System.out.println("Machine stopped.");
-            default:
-                System.out.println("Command not recognized");
+        int[][] grid = {
+            {3, 5, 2343 },
+            {2, 4 },
+            {1, 2, 3, 4}
+        };
+
+        System.out.println(grid[1][1]);
+        System.out.println(grid[0][2]);
+
+        String[][] texts = new String[2][3];
+
+        texts[0][0] = "Hello there";
+
+        System.out.println(texts[0][0]);
+
+        for(int row = 0; row < grid.length; row++) {
+            for(int col = 0; col < grid[row].length; col++) {
+                System.out.print(grid[row][col] + "\t");
+            }
+            System.out.println();
         }
 
+        String[][] words = new String[2][];
+
+        System.out.println(words[0]);
+
+        words[0] = new String[3];
+        words[0][1] = "hi there";
+
+        System.out.println(words[0][1]);
     }
 }
